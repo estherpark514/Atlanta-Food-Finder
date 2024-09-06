ba1d9ca147a283eebe51341a04113ed24ecacd32
Esther Park <esther514514@gmail.com>
2024-09-06 12:31:18 -0400

    customize user admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import PasswordReset
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'username')
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'first_name', 'last_name', 'email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(PasswordReset)
