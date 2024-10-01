4add268160185be90addd6715df7f28bc627e7b0
Esther Park <esther514514@gmail.com>
2024-10-01 13:46:48 -0400

    Frontend/Backend Update
    cuisine type, distance, google reviews should be fixed

--- reviews/services.py
+++ reviews/services.py
@@ -23,3 +23,23 @@ def fetch_restaurants_from_api(api_key: str, location: str, radius: int):
     except requests.exceptions.RequestException as e:
         # print(f"Request failed: {e}")
         return []
+
+def fetch_place_details(place_id: str, api_key: str):
+    url = "https://maps.googleapis.com/maps/api/place/details/json"
+    params = {
+        'place_id': place_id,
+        'key': api_key
+    }
+
+    try:
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            json_response = response.json()
+            # print(f"Fetch place details API Response JSON: {json_response}") 
+            return response.json().get('result', {})
+        else:
+            return {}
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed - fetch place details: {e}")
+        return {}
